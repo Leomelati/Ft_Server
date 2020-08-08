6d8d6f37e5b3a565b66df966b729ec47cc5795d9
Lmartins <lmartins@42sp.org.br>
2020-08-08 05:35:44 +0200

    build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
 docker rm $(docker ps -a -f status=exited -q)
 docker rmi ft_server
-docker build -t ft_server .
+docker build -t ft_server ../
 docker run --name ft_server -it -p 80:80 -p 443:443 ft_server
